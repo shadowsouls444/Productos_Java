09c2f91bc907894550fe82eac8701f8dd8b76b27
Sebas5158R <sebastianriveraa4@gmail.com>
2025-03-12 00:52:06 -0500

    Crud productos con BD

--- src/Services/ProductoService.java
+++ src/Services/ProductoService.java
@@ -2,10 +2,8 @@ package Services;
 
 import DB.DataBase;
 import Models.Producto;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Date; 
+
+import java.sql.*;
 
 public class ProductoService {
     
@@ -15,7 +13,7 @@ public class ProductoService {
         String sql = "INSERT INTO productos(id, nombreProducto, categoria, fechaVencimiento, cantidad, precio) VALUES (?, ?, ?, ?, ?, ?)";
         
         try(PreparedStatement stmt = conexion.prepareStatement(sql)) {
-            
+
             stmt.setInt(1, producto.getId());
             stmt.setString(2, producto.getNombreProducto());
             stmt.setInt(3, producto.getCategoria().getId());
@@ -24,15 +22,84 @@ public class ProductoService {
             stmt.setDouble(6, producto.getPrecio());
             stmt.executeUpdate();
             System.out.println("El producto se registro correctamente");
-            
+
         } catch(SQLException e) {
-            
+
             System.out.println("ERROR: Al registrar producto " + e.getMessage());
-            
+
         } finally {
-            
+
             DataBase.DesconectarDB(conexion);
-            
+
+        }
+    }
+
+    // Editar producto
+    public void EditarProducto(Producto producto) {
+        Connection conexion = DataBase.Conectar();
+        String sql = "UPDATE productos SET nombre = ?, categoria = ?, fechaVencimiento = ?, cantidad = ?, precio = ? WHERE id = ?";
+
+        try(PreparedStatement preparedStatement = conexion.prepareStatement(sql)) {
+            preparedStatement.setString(1, producto.getNombreProducto());
+            preparedStatement.setInt(2, producto.getCategoria().getId());
+            preparedStatement.setDate(3, (Date) producto.getFechaVencimiento());
+            preparedStatement.setInt(4, producto.getCantidad());
+            preparedStatement.setDouble(5, producto.getPrecio());
+            preparedStatement.executeUpdate();
+            System.out.println("El producto se ha modificado correctamente");
+        } catch (SQLException e) {
+            System.out.println("ERROR: Al editar producto " + e.getMessage());
+        } finally {
+            DataBase.DesconectarDB(conexion);
+        }
+    }
+
+    // Mostrar productos registrados
+    public void MostrarProductos() {
+        Connection conexion = DataBase.Conectar();
+        String sql = "SELECT * FROM productos";
+
+        try(PreparedStatement preparedStatement = conexion.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt(1);
+                String nombre = resultSet.getString(2);
+                int idCategoria = resultSet.getInt(3);
+                String fechaVencimiento = resultSet.getString(4);
+                int cantidad = resultSet.getInt(5);
+                double precio = resultSet.getDouble(6);
+
+                System.out.println("PRODUCTOS REGISTRADOS EN LA BASE DE DATOS");
+                System.out.println("------------------------");
+                System.out.println("ID: " + id);
+                System.out.println("NOMBRE: " + nombre);
+                System.out.println("ID_CATEGORIA: " + idCategoria);
+                System.out.println("FECHA_VENCIMENTO: " + fechaVencimiento);
+                System.out.println("CANTIDAD: " + cantidad);
+                System.out.println("PRECIO: " + precio);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("ERROR: Al mostrar productos " + e.getMessage());
+        } finally {
+            DataBase.DesconectarDB(conexion);
+        }
+    }
+
+    //Eliminar producto
+    public void EliminarProducto(int id) {
+        Connection conexion = DataBase.Conectar();
+        String sql = "DELETE FROM productos WHERE id = ?";
+
+        try(PreparedStatement preparedStatement = conexion.prepareStatement(sql)) {
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+            System.out.println("El producto se ha eliminado correctamente");
+        } catch (SQLException e) {
+            System.out.println("ERROR: Al eliminar producto " + e.getMessage());
+        } finally {
+            DataBase.DesconectarDB(conexion);
         }
     }
     
