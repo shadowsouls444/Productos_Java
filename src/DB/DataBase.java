dbbed388edfc11d4d4fd19ac8a2eedae6ded13a1
Sebas5158R <sebastianriveraa4@gmail.com>
2025-04-06 13:58:00 -0500

    Remove password for db

--- src/DB/DataBase.java
+++ src/DB/DataBase.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DataBase {
     private static final String url = "jdbc:mysql://localhost:3306/inventario";
     private static final String usuario = "root";
-    private static final String password = "200530";
+    private static final String password = "";
     
     public static Connection Conectar() {
         Connection conexion = null;
